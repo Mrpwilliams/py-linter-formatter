c6816dee3db64baf3a9c715cbf1f9860ada15619
Mrpwilliams <65180628+Mrpwilliams@users.noreply.github.com>
2026-02-14 16:51:49 +0000

    Third attempt

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line":error["line_number"],
+    return {"line": error["line_number"],
             "column": error["column_number"],
             "message": error["text",],
-            "name":error["code"],
+            "name": error["code"],
             "source":"flake8"}
 
 
