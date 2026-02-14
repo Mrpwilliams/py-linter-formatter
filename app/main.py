d691ff823ee948bea63d4a892250df352f62955e
Mrpwilliams <65180628+Mrpwilliams@users.noreply.github.com>
2026-02-14 17:53:48 +0000

    again

--- app/main.py
+++ app/main.py
@@ -1,23 +1,17 @@
 def format_linter_error(error: dict) -> dict:
-    return {
-        "line": error["line_number"],
+    return {"line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8",
-    }
+        "source": "flake8",}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {
-        "errors": [format_linter_error(error=e) for e in errors],
+    return {"errors": [format_linter_error(error=e) for e in errors],
         "path": file_path,
-        "status": "failed" if errors else "passed",
-    }
+        "status": "failed" if errors else "passed",}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        format_single_linter_file(file_path=path, errors=errs)
-        for path, errs in linter_report.items()
-    ]
+    return [format_single_linter_file(file_path=path, errors=errs)
+        for path, errs in linter_report.items()]
